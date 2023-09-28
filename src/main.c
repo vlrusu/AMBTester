bd097aebcdb4b2aded2461d94abee3ec2b78555a
Vadim Rusu <vadim.l.rusu@gmail.com>
2023-09-28 00:27:40 -0500

    update

--- src/main.c
+++ src/main.c
@@ -38,8 +38,7 @@
 
 static const uint16_t CAL_MASK = 0xffff;
 
-#define AIRCR_Register (*((volatile uint32_t*)(PPB_BASE + 0x0ED0C)))
-
+#define AIRCR_Register (*((volatile uint32_t *)(PPB_BASE + 0x0ED0C)))
 
 // Voltage divider ratio
 #define DIVIDER_RATIO 3.0f
@@ -50,10 +49,12 @@ static const uint16_t CAL_MASK = 0xffff;
 // ADC Resolution
 #define ADC_RESOLUTION (1 << 12) // 12-bit ADC
 
-static const float full_battery = 4.2; // these are our reference voltages for a full / empty battery, in volts
+static const float full_battery = 4.9; // these are our reference voltages for a full / empty battery, in volts
 static const float empty_battery = 2.8;
 
 static bool cleartogo;
+
+UWORD *BlackImage;
 
 // Define a pair of int values
 typedef struct
@@ -82,16 +83,41 @@ typedef struct
 
 // Define a map as an array of key-value pairs. First is the dut, second is the ground ref
 
-KeyValuePair bridgeMap[] = {
+// CAL side
+KeyValuePair bridgeMapCAL[] = {
+    {0, {8,4}, {4,14} }, //vcc vcc 2+7, P0-P11 self check
+{1, {5,1}, {16,17} }, //calsiga vcalab 11+13, P13-P14
+{2, {4,2}, {15,16} }, //calsigb calsiga 9+11, P12-P13
+{3, {3,3}, {14,15} }, //vcc calsigb 7+9, P11-P12
+{4, {3,5}, {14,13} }, //vcc gndcal 7+5, P11-P10
+{5, {0,6}, {11,12} }, //sigbp gnd 1+3, P8-P9
+{6, {12,7}, {10,11} }, //sigbn sigbp 10+1, P4-P8
+{7, {1,11}, {9,10} }, //gnd sigbn 3+10, P9-P4
+{8, {13,6}, {7,8} }, //sigap gnd 12+3, P5-P9
+{9, {14,10}, {6,7} }, //sigan sigap 14+12, P6-P5
+{10, {1,9}, {5,6} }, //gnd sigan 3+14, P9-P6
+{11, {1,15}, {5,4} }, //gnd vcc 3+2, P9-P0
+{12, {9,15}, {3,4} }, //sck vcc 4+2, P1-P0
+{13, {10,14}, {2,3} }, //sdi sck 6+4, P2-P1
+{14, {11,13}, {1,2} }, //cs sdi 8+6, P3-P2
+};
+
+
+
+KeyValuePair bridgeMapHV[] = {
     {0, {10, 12}, {2, 1}}, // csbar+sdi 6+8, P2-P3
     {1, {10, 14}, {2, 3}}, // sdi sck 6+4, P2-P1
     {2, {8, 4}, {4, 14}},  // vcc vcc 2+7, P0-P11
-    {3, {9, 15}, {3, 4}},  // sck vcc 4+2, P1-P0
 
 };
 
 // Define the size of the map based on the number of entries
-const size_t intMapSize = sizeof(bridgeMap) / sizeof(bridgeMap[0]);
+const size_t intMapSizeCAL = sizeof(bridgeMapCAL) / sizeof(bridgeMapCAL[0]);
+const size_t intMapSizeHV = sizeof(bridgeMapHV) / sizeof(bridgeMapHV[0]);
+
+
+KeyValuePair *bridgeMap;
+size_t intMapSize;
 
 // Function to get a pair of values from the map given a key
 /* IntPair* getValues(int key) { */
@@ -123,28 +149,43 @@ float read_vsys_voltage()
     return voltage * DIVIDER_RATIO;
 }
 
+void AMBCAL_setup()
+{
+    Paint_ClearWindows(1, 50, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, WHITE);
+    Paint_DrawString_EN(1, 20, "Set for AMBCAL", &Font20, 0x000f, 0xfff0);
+    // AIRCR_Register = 0x5FA0004;
+    cleartogo = 0;
+    bridgeMap = bridgeMapCAL;
+    intMapSize = intMapSizeCAL;
+    LCD_1IN14_Display(BlackImage);
+}
+
+void AMBHV_setup()
+{
+
+    Paint_ClearWindows(1, 50, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, WHITE);
+    Paint_DrawString_EN(1, 20, "Set for  AMBHV", &Font20, 0x000f, 0xfff0);
+    // AIRCR_Register = 0x5FA0004;
+    cleartogo = 0;
+    bridgeMap = bridgeMapHV;
+    intMapSize = intMapSizeHV;
+    LCD_1IN14_Display(BlackImage);
+}
+
 // This function will be called when GPIO 5 goes high
 void gpio_callback(uint gpio, uint32_t events)
 {
     if (gpio == KEY_A)
     {
         // Your custom logic here
+        AMBCAL_setup();
 
-        Paint_ClearWindows(1, 20, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, WHITE);
-        Paint_DrawString_EN(1, 100, "Set for AMBCAL", &Font20, 0x000f, 0xfff0);
-        AIRCR_Register = 0x5FA0004;
-        cleartogo = 0;
-        LCD_1IN14_Display(BlackImage);
         printf("gpio =%d\r\n", KEY_A);
     }
 
     else if (gpio == KEY_B)
     {
-        Paint_ClearWindows(1, 20, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, WHITE);
-        Paint_DrawString_EN(1, 100, "Set for AMBHV", &Font20, 0x000f, 0xfff0);
-        AIRCR_Register = 0x5FA0004;
-        cleartogo = 0;
-        LCD_1IN14_Display(BlackImage);
+        AMBHV_setup();
         printf("gpio =%d\r\n", KEY_B);
     }
 }
@@ -159,7 +200,7 @@ void core1_entry()
         float vsys_voltage = read_vsys_voltage();
         char str_float[20]; // Enough space for a float and null-terminator
         sprintf(str_float, "%.2f", vsys_voltage);
-        printf("VSYS Voltage: %.2fV %s\n", vsys_voltage, charging_state ? "Charging" : "not charging");
+        printf("VSYS Voltage: %.2f \n", vsys_voltage);
         Paint_DrawString_EN(1, 1, str_float, &Font20, 0x000f, 0xfff0);
 
         if (vsys_voltage < empty_battery)
@@ -224,7 +265,7 @@ int main()
 
     // LCD_SetBacklight(1023);
     UDOUBLE Imagesize = (LCD_1IN14_HEIGHT)*LCD_1IN14_WIDTH * 2;
-    UWORD *BlackImage;
+
     if ((BlackImage = (UWORD *)malloc(Imagesize)) == NULL)
     {
         printf("Failed to apply for black memory...\r\n");
@@ -241,38 +282,46 @@ int main()
     SET_Infrared_PIN(KEY_B);
 
     // Launch core1_entry() on core 1
-    multicore_launch_core1(core1_entry);
+    //    multicore_launch_core1(core1_entry);
 
     // Set up the interrupt to trigger when the button goes from high to low (a falling edge),
     // which corresponds to the button being pressed
     gpio_set_irq_enabled_with_callback(KEY_A, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
     gpio_set_irq_enabled_with_callback(KEY_B, GPIO_IRQ_EDGE_FALL, true, &gpio_callback);
 
+    AMBCAL_setup();
+
     // Wait forever
     while (1)
     {
 
-        /*       if (DEV_Digital_Read(KEY_A) == 0)
-              {
-                  Paint_ClearWindows(1, 20, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, WHITE);
-                  Paint_DrawString_EN(1, 100, "Set for AMBCAL", &Font20, 0x000f, 0xfff0);
-                  cleartogo = 0;
-                  LCD_1IN14_Display(BlackImage);
-                  printf("gpio =%d\r\n", KEY_A);
-                  DEV_Delay_ms(1000);
-                  // set for AMBCAL
-              } */
+       
+
+        
+
+        // Read VSYS voltage
+        float vsys_voltage = read_vsys_voltage();
+        char str_float[20]; // Enough space for a float and null-terminator
+        sprintf(str_float, "%d%%", (int) (100 * vsys_voltage / full_battery));
+        printf("VSYS Voltage: %.2f \n", vsys_voltage);
+        Paint_DrawString_EN(1, 1, str_float, &Font20, 0x000f, 0xfff0);
+
+        if (vsys_voltage < empty_battery)
+        {
+            Paint_DrawString_EN(100, 1, "Low batt", &Font20, 0x000f, 0xfff0);
+        }
+        LCD_1IN14_Display(BlackImage);
 
         // wait for selfcheck to go low
         adg706_set_address(&dut, bridgeMap[0].mbpair.first);
         adg706_set_address(&gndref, bridgeMap[0].mbpair.second);
-
-        sleep_ms(10);
+        sleep_ms(150);
 
         bool pin_state = gpio_get(INPUT_PIN);
         if (pin_state == 0)
         {
-            Paint_ClearWindows(1, 20, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, RED);
+            Paint_ClearWindows(1, 40, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, YELLOW);
+            Paint_DrawString_EN(1, 80, "Waiting for AMB", &Font20, 0x000f, 0xfff0);
             LCD_1IN14_Display(BlackImage);
             cleartogo = 1;
             continue;
@@ -293,20 +342,19 @@ int main()
                 adg706_set_address(&dut, pin1);
                 adg706_set_address(&gndref, pin2);
 
-                sleep_ms(10);
+                sleep_ms(150);
 
                 bool pin_state = gpio_get(INPUT_PIN);
-                bool charging_state = gpio_get(CHARGING_PIN);
 
                 // Print the state of the pin
                 printf("Pin state is: %s ", pin_state ? "High" : "Low");
                 printf(" between pins %d and %d \n", bridgeMap[i].dbpair.first, bridgeMap[i].dbpair.second);
-                sleep_ms(10);
 
                 if (pin_state == 1)
                 {
-                    Paint_ClearWindows(1, 20, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, RED);
-                    Paint_DrawString_EN(1, 50, "Bridge detected", &Font20, 0x000f, 0xfff0);
+                    Paint_ClearWindows(1, 40, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, RED);
+                    sprintf(str_float, "Bridge  %d - %d", bridgeMap[i].dbpair.first, bridgeMap[i].dbpair.second);
+                    Paint_DrawString_EN(1, 70, str_float, &Font20, 0x000f, 0xfff0);
                     LCD_1IN14_Display(BlackImage);
                     cleartogo = 0;
                     break;
@@ -316,8 +364,8 @@ int main()
                 { // if we got here turn allto green
 
                     // turn to green
-                    Paint_ClearWindows(1, 20, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, GREEN);
-                    Paint_DrawString_EN(1, 50, "All ok", &Font20, 0x000f, 0xfff0);
+                    Paint_ClearWindows(1, 24, LCD_1IN14.WIDTH, LCD_1IN14.HEIGHT, GREEN);
+                    Paint_DrawString_EN(1, 70, "All ok", &Font20, 0x000f, 0xfff0);
                     printf("All ok");
                     LCD_1IN14_Display(BlackImage);
                     cleartogo = 0;
